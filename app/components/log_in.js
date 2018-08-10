a23937fb776a9cd9ba2651ec6ad5eba6ed3b82c2
White Cloak <whitecloak@Whites-MacBook-Pro.local>
2018-08-10 18:07:08 +0800

    updated login component

--- app/components/log_in.js
+++ app/components/log_in.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Button, Alert } from 'react-native';
+import { Text, View, StyleSheet, Button, Alert, TextInput } from 'react-native';
 
 export default class LogIn extends Component {
     render() {
         return (
             <View style={styles.container}> 
-                <View> 
-                    <Text style={styles.topContainer}> Login To Reddit </Text> 
+                <View style={styles.topContainer}> 
+                    <View>
+                        <Text style={{color: 'white', fontSize: 18}}> Login To Reddit </Text> 
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <TextInput style={styles.inputStyle} placeholder="Username" />
+                        <TextInput style={styles.inputStyle} placeholder="Password" />
+                    </View>
+                    <View style={styles.bottomTextContainer}>
+                        <Text style={{color: "#0074CD"}}> SIGN UP </Text>
+                        <Text style={{color: "#0074CD"}}> FORGOT PASSWORD </Text>
+                    </View>
                 </View>
                 <View style={styles.buttonContainer}>
                     <Button onPress = {() => Alert.alert("Hello")} title={"LOG IN"}/> 
@@ -25,14 +35,37 @@ const styles = StyleSheet.create({
         padding: 15
     },
     topContainer: {
-        color: '#fff',
-        textAlign: 'left'
+        width: '100%',
+        // borderWidth: 1,
+        // borderColor: "#fff",
+        // borderStyle: "solid"
     },
     buttonContainer: {
         // borderWidth: 1,
         // borderColor: 'white',
         // borderStyle: 'solid',
         width: '85%',
-        // paddingBottom: 35
+        paddingBottom: 35
+    },
+    inputContainer: {
+        paddingTop : 35,
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+    inputStyle: {
+        height: 48,
+        borderBottomWidth: 1,
+        borderBottomColor: '#0074CD',
+        borderStyle: 'solid',
+        color: 'white'
+    },
+    bottomTextContainer: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop : 25,
+        paddingLeft: 10,
+        paddingRight: 10
     }
 })
